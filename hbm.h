f50489176e4dd174e56eceeafd1e415391501204
xinz <xinz@gekang.com>
2017-02-21 22:47:02 +0800

    init version

---
+++ hbm.h
@@ -0,0 +1,22 @@
+/*
+ +----------------------------------------------------------------------+
+ | Author: Xingzhi Liu  <dudubird2006@163.com>                          |
+ +----------------------------------------------------------------------+
+ */
+
+#ifndef HBM_H
+#define HBM_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+unsigned char * hbm_patternmatch(const unsigned char *text,int n,
+                                            const unsigned char *pat,int m,
+                                            int no_case,int codetype);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
